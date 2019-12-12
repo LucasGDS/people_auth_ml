d78f3b4b41752a10c4f18761668a2c6919c64c19
LucasGDS <LucasGDS@users.noreply.github.com>
2019-12-12 10:52:21 -0300

    alpha
    * add keras and facenet to identification

--- qtmlid.py
+++ qtmlid.py
@@ -15,6 +15,9 @@ import librosa
 from faced import FaceDetector #for better cpu support
 from faced.utils import annotate_image
 
+from keras.models import Model, Sequential, model_from_json
+from keras import optimizers
+
 # from PyQt5.QtCore import Qt, QTimer
 
 # app = QApplication([])
@@ -36,6 +39,20 @@ embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
 train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
 
 face_detector = FaceDetector()
+
+# load json and create model
+json_file = open('modelnochanges.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("modelregister-2019-10-15-16:52.h5")
+print("Loaded model from disk")
+
+networkoptimizer = optimizers.SGD(lr=0.01)
+loaded_model.compile(loss='binary_crossentropy',
+              optimizer=networkoptimizer,
+              metrics=['accuracy'])
 
 #Funcao que calcula a distancia euclidiana entre dois vetores
 def distance(vector1, vector2):
@@ -73,20 +90,46 @@ def who_is_it_crop(img, database):
     #Calculando o embedding do visitante
     visitor = get_embedding_img(img)
     #Calculando a distacia do visitante com os demais funcionarios
-    res_vec = []
+    
     for name, employee in database.items():
         dist = distance(visitor, employee)
-        res_vec.append(dist)
         if dist < min_dist:
             min_dist = dist 
             identity = name
     #verificando a identidade
     if min_dist > 0.5:
         print("Essa pessoa nao esta cadastrada!")
-        return None, img, res_vec
+        return None, img
     else:
-        return identity, img, res_vec
+        return identity, img
 
+def whose_voice(visitor, database):
+    max_sim = 0 
+    identity = -1
+
+    xp1 = np.array( [visitor,] )
+
+    X_test_l = xp1.reshape(*xp1.shape, 1)
+
+    #Calculando o embedding do visitante
+    #Calculando a distacia do visitante com os demais funcionarios
+    for name, employee in database.items():
+        xp2 = np.array( [employee,] )
+        X_test_r = xp2.reshape(*xp2.shape, 1)
+
+        y_pred = loaded_model.predict([X_test_l, X_test_r])
+
+        print ("y_pred:",y_pred)
+        similarity = y_pred[0]
+        if similarity > max_sim:
+            max_sim = similarity 
+            identity = name
+    #verificando a identidade
+    if max_sim < 0.99:
+        print("Essa pessoa nao esta cadastrada!")
+        return None
+    else:
+        return identity
 
 class Mlid(QtWidgets.QMainWindow, qtmliddes.Ui_MainWindow):
     
@@ -99,6 +142,7 @@ class Mlid(QtWidgets.QMainWindow, qtmliddes.Ui_MainWindow):
         self.set_trocas(0)
         self.pushButton_cad.clicked.connect(self.buttoncad_press)
         self.pushButton_rec.clicked.connect(self.buttonrec_press)
+        self.pushButton_id.clicked.connect(self.buttonid_press)
         self.tabWidget.currentChanged.connect(self.onUpdate)
         self.listWidget.currentTextChanged.connect(self.listChange)
         # cadastrarview.
@@ -112,10 +156,15 @@ class Mlid(QtWidgets.QMainWindow, qtmliddes.Ui_MainWindow):
         self.timer_id = QtCore.QTimer(self)
         self.timer_id.timeout.connect(self.id_routine)
 
+        self.timer_id_voicerecord = QtCore.QTimer(self)
+        self.timer_id_voicerecord.timeout.connect(self.id_voice)
+
         self.database_img = {}
         self.database_snd = {}  
         self.id_state = 0
-    
+        self.soundprocessingflag = False
+        # self.id_rgb_frame = None
+            
     def set_trocas(self,valor):
         self.trocas = valor
         scenecadastrar = QGraphicsScene()
@@ -187,11 +236,34 @@ class Mlid(QtWidgets.QMainWindow, qtmliddes.Ui_MainWindow):
             user = "default"
         # elif user == None:
         #     continue 
-            self.prepare_id()
         print(user)
         comando = ["arecord", "--duration", "4", "--format", "cd", "./voice/"+user+".wav"]
         gravacao=Popen(comando)
         self.progressbartimer.start(1000)        
+
+    def buttonid_press(self):
+        comando = ["arecord", "--duration", "4", "--format", "cd", "./id/idrecord.wav"] #TODO:runtime filename creation 
+        gravacao=Popen(comando)
+        self.id_state = 1
+        self.timer_id_voicerecord.start(5000)
+        
+        ret, frame = cap.read()
+        if frame.shape[0] == 0:
+            pass
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+        bboxes = face_detector.predict(rgb_frame, thresh)
+        if len(bboxes) != 0:
+            for x,y,w,h,p in bboxes :
+                ####
+                x0=int(x-w/2)
+                y0=int(y-h/2)
+                x0plusw = int(x+w/2)
+                y0plush = int(y+h/2)
+                ####
+                cv2.imshow("window", frame[y0: y0plush,x0 : x0plusw])
+                identity, image = who_is_it_crop(rgb_frame[y0: y0plush,x0 : x0plusw], self.database_img)
+                print ("Facenet: ",identity)
+        pass
     
     def onUpdate(self):
         tabWidget = self.tabWidget
@@ -252,24 +324,40 @@ class Mlid(QtWidgets.QMainWindow, qtmliddes.Ui_MainWindow):
                         x0plusw = int(x+w/2)
                         y0plush = int(y+h/2)
                         ####
-                        cv2.imshow(path[:-4], img[y0: y0plush,x0 : x0plusw])
+                        # cv2.imshow(path[:-4], img[y0: y0plush,x0 : x0plusw])
                 print('loaded '+path)
                 self.database_img[path[:-4]] = get_embedding_img(rgb_img[y0: y0plush,x0 : x0plusw])
                 audio, sr = librosa.load('./voice/'+path[:-4]+".wav", sr=22000, duration=2, mono=True)
                 processed = librosa.feature.melspectrogram(y=audio, sr=sr)
-            if (processed.shape == (128, 86)):
-               self.database_snd[path[:-4]] = processed
+                if (processed.shape == (128, 86)):
+                    self.database_snd[path[:-4]] = processed
+        print (self.database_snd)
         
 
     def id_routine(self):
-
         if self.id_state == 0:
             self.draw_reg()
             pass
-        elif self.id_state == 1:
-            pass
-        elif self.id_state == 2:
-            pass
+        elif self.id_state == 1: # recording voice, changed in buttonid_press():
+            # self.timer_id.stop()
+            self.soundprocessingflag == True
+            while self.soundprocessingflag == True:    
+                if self.id_state == 2: # processing
+
+                    pass
+                elif self.id_state == 3: # results
+
+                    pass
+    
+    def id_voice(self):
+        self.timer_id_voicerecord.stop()
+        audio, sr = librosa.load('./id/idrecord.wav', sr=22000, duration=2, mono=True)
+        processed = librosa.feature.melspectrogram(y=audio, sr=sr)
+
+        identity = whose_voice(processed, self.database_snd)
+        print("Voice: ",identity)
+        pass
+
 
 def main():
     if not(os.path.exists('dataset')):
@@ -278,6 +366,9 @@ def main():
     if not(os.path.exists('voice')):    # True
         print("creating voices folder")
         os.mkdir('voice')
+    if not(os.path.exists('id')):    # True
+        print("creating id folder")
+        os.mkdir('id')
     app = QApplication(sys.argv)
     form = Mlid()
     form.show()
